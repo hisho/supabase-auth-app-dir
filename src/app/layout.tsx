8451112d23357babb7e4aa4ca9e6822a0440dcb1
hisho <hisho.web@gmail.com>
2023-06-21 00:55:05 +0900

    ✨ update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from 'react'
 
 export const metadata = {
-  description: 'next app',
-  title: 'Next App',
+  description: 'Supabase Auth with Next.js App Directory',
+  title: 'Supabase Auth with Next.js App Directory',
 }
 
 type Props = {
@@ -11,7 +11,7 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body>{children}</body>
     </html>
   )
