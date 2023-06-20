24bd1a843d481586411f2e36fe249f8491fc8fff
hisho <hisho.web@gmail.com>
2023-06-21 01:57:55 +0900

    ✨ wrap spacer component

---
+++ src/component/spacer/spacer.tsx
@@ -0,0 +1,33 @@
+import { memo } from 'react'
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as const
+type List = (typeof list)[number]
+
+type Props = {
+  isHorizontal?: boolean
+  size?: List
+}
+
+const resolveValue = (value: List | undefined) => {
+  if (value === undefined) {
+    return undefined
+  } else if (list.includes(value)) {
+    return value * 4
+  }
+  return undefined
+}
+export const Spacer = memo(({ isHorizontal = false, size }: Props) => {
+  return (
+    <span
+      className={`block select-none pointer-events-none ${
+        isHorizontal ? 'flex-1' : ''
+      }`}
+      style={{
+        height: isHorizontal ? undefined : resolveValue(size),
+        width: isHorizontal ? resolveValue(size) : undefined,
+      }}
+      aria-hidden={'true'}
+      tabIndex={-1}
+    />
+  )
+})
