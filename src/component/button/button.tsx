32f3e4fe125e1d99cf842595b022ff4ddbc85c8b
hisho <hisho.web@gmail.com>
2023-06-21 04:17:55 +0900

    ✨ update

--- src/component/button/button.tsx
+++ src/component/button/button.tsx
@@ -1,11 +1,27 @@
+import { cn } from '@/lib/utils/cn'
 import { ComponentProps } from 'react'
 
-type Props = Omit<ComponentProps<'button'>, 'className'>
+type Props = Omit<ComponentProps<'button'>, 'disabled'> & {
+  isLoading?: boolean
+}
 
-export const Button = ({ children, ...props }: Props) => {
+export const Button = ({
+  children,
+  className,
+  isLoading = false,
+  ...props
+}: Props) => {
   return (
-    <button className={'btn btn-outline btn-primary'} {...props}>
-      {children}
+    <button
+      className={cn('btn btn-outline btn-primary w-full', className)}
+      {...props}
+      disabled={isLoading}
+    >
+      {isLoading ? (
+        <span className={'loading loading-spinner loading-xs'} />
+      ) : (
+        children
+      )}
     </button>
   )
 }
