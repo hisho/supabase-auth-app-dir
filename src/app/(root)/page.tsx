0e0102e328177e25f80603c92067cf99f62b7ef7
hisho <hisho.web@gmail.com>
2023-06-21 01:11:35 +0900

    🔥 remove sign out button from page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,4 @@
 import { SignInForm } from '@/feature/auth/sign-In/sign-In-form'
-import { useSignOut } from '@/feature/auth/sign-out/use-sign-out'
 import { SignUpForm } from '@/feature/auth/sign-up/sign-up-form'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
@@ -7,15 +6,11 @@ import { cookies } from 'next/headers'
 const Page = async () => {
   const spabase = createServerComponentClient({ cookies })
   const user = await spabase.auth.getUser()
-  const { handleSignOut } = useSignOut()
 
   return (
     <div>
       <SignUpForm />
       <SignInForm />
-      <button onClick={handleSignOut} type={'button'}>
-        サインアウト
-      </button>
       {JSON.stringify(user.data.user)}
     </div>
   )
