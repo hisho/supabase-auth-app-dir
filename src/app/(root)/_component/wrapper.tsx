f200b0d7d3bbe1d830bcfea90d5baf74f1645dd1
hisho <hisho.web@gmail.com>
2023-06-21 02:31:00 +0900

    ✨ update wrapper

--- src/app/(root)/_component/wrapper.tsx
+++ src/app/(root)/_component/wrapper.tsx
@@ -1,9 +1,14 @@
-import { ReactElement } from 'react'
+import { cn } from '@/lib/utils/cn'
+import { ComponentProps, ReactElement } from 'react'
 
 type Props = {
   children: Iterable<ReactElement> | ReactElement
-}
+} & Pick<ComponentProps<'div'>, 'className'>
 
-export const Wrapper = ({ children }: Props) => {
-  return <div className={'mx-auto w-full max-w-3xl px-5'}>{children}</div>
+export const Wrapper = ({ children, className }: Props) => {
+  return (
+    <div className={cn('mx-auto w-full max-w-3xl px-5', className)}>
+      {children}
+    </div>
+  )
 }
