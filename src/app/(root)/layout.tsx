2597b30ac154840bf86756a8311383d54d9deeba
hisho <hisho.web@gmail.com>
2023-06-21 03:36:35 +0900

    ✨ add auth provider

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from '@/app/(root)/_component/footer'
 import { Header } from '@/app/(root)/_component/header'
 import { Wrapper } from '@/app/(root)/_component/wrapper'
+import { AuthProvider } from '@/feature/auth/auth-provider/auth-provider'
 import { ReactElement } from 'react'
 
 type Props = {
@@ -9,11 +10,13 @@ type Props = {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className={'h-[100svh] grid grid-rows-[auto,1fr,auto]'}>
-      <Header />
-      <Wrapper className={'pt-10 pb-14'}>{children}</Wrapper>
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div className={'h-[100svh] grid grid-rows-[auto,1fr,auto]'}>
+        <Header />
+        <Wrapper className={'pt-10 pb-14 relative'}>{children}</Wrapper>
+        <Footer />
+      </div>
+    </AuthProvider>
   )
 }
 
