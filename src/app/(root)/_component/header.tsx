578584b15ea238d4331c4a7167cc7c110c6e17dd
hisho <hisho.web@gmail.com>
2023-06-21 02:03:57 +0900

    💄 update header ui

--- src/app/(root)/_component/header.tsx
+++ src/app/(root)/_component/header.tsx
@@ -1,16 +1,26 @@
 'use client'
 
+import { Wrapper } from '@/app/(root)/_component/wrapper'
+import { Spacer } from '@/component/spacer/spacer'
 import { useSignOut } from '@/feature/auth/sign-out/use-sign-out'
+import Link from 'next/link'
 
 export const Header = () => {
   const { handleSignOut } = useSignOut()
 
   return (
-    <header>
-      header
-      <button onClick={handleSignOut} type={'button'}>
-        サインアウト
-      </button>
+    <header className={'bg-black py-2'}>
+      <Wrapper>
+        <div className={'flex'}>
+          <Link href={'/'}>
+            <div className={'text-3xl'}>📦</div>
+          </Link>
+          <Spacer isHorizontal />
+          <button onClick={handleSignOut} type={'button'}>
+            サインアウト
+          </button>
+        </div>
+      </Wrapper>
     </header>
   )
 }
